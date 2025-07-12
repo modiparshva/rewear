3fef8867d4fa3f9a6a1415b22e92702b951ad4a5
Rohit H Solanki <solanki.rohit6105@gmail.com>
2025-07-12 12:43:33 +0530

    metadata change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,59 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
+const keywords = [
+  "rewear",
+  "sustainable fashion",
+  "clothing exchange",
+  "swap clothes",
+  "eco-friendly fashion",
+  "secondhand clothing",
+  "fashion marketplace",
+  "community fashion",
+  "sustainable clothing",
+  "clothing swaps",
+  "recycle fashion",
+  "upcycled clothing",
+  "fashion sustainability",
+  "ethical fashion",
+  "fashion sharing",
+  "circular fashion",
+  "fashion reuse",
+  "clothing donation",
+  "fashion community",
+  "fashion trade",
+  "fashion collaboration",
+  "fashion innovation",
+  "fashion technology",
+  "fashion startups",
+  "fashion trends",
+  "fashion events",
+  "fashion workshops",
+  "fashion education",
+  "fashion awareness",
+]
+
+const authors = [
+  { 
+    name: "Rohit Solanki", 
+    url: "https://github.com/Rohit-Solanki-6105"
+  },
+  { 
+    name: "Dishant Dyavarchetti", 
+    url: "https://github.com/Dishant-dyavarchetti"
+  },
+  { 
+    name: "Parsva Modi", 
+    url: "https://github.com/modiparshva"
+  },
+]
+
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Rewear',
+  description: 'Created by RDP3389 Group',
+  keywords: keywords,
+  authors: authors,
+  creator: "RDP3389 Group",
 }
 
 export default function RootLayout({
